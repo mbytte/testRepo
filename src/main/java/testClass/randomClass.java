48fb3433ab28ba87f2afefacd97dc54439ffe741
Meganl <Meganl@ZAKZRHB-CLLB14.ballito.local>
2022-07-28 12:27:13 +0200

    Made a thing

---
+++ src/main/java/testClass/randomClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testClass;
+
+/**
+ *
+ * @author Meganl
+ */
+public class randomClass 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("RiiiiiiIIiIIIIIIIISSSSSEEEEE");
+    }
+}
